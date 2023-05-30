ae67b04c445b345224ffba7eea9f77798b7c653a
Felipe Rafael Barbosa <rafaelt.ibarbosa@gmail.com>
2023-05-30 15:50:42 -0300

    first commit

---
+++ dijkstra.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define V 6
+
+// Grafo representado por uma Matriz de Adjacência.
+int grafo[V][V] = {
+  {0, 4, 0, 0, 0, 0},
+  {4, 0, 8, 0, 0, 0},
+  {0, 8, 0, 7, 0, 4},
+  {0, 0, 7, 0, 9, 14},
+  {0, 0, 0, 9, 0, 10},
+  {0, 0, 4, 14, 10, 0}
+};
+
+int distancias[V];
+int visitados[V];
+
+int encontrarVerticeMaisProximo() {
+  int distanciaMinima = INT_MAX;
+  int verticeMaisProximo;
+
+  for (int i = 0; i < V; i++) {
+    if (!visitados[i] && distancias[i] <= distanciaMinima) {
+      distanciaMinima = distancias[i];
+      verticeMaisProximo = i;
+    }
+  }
+
+  return verticeMaisProximo;
+}
+
+
+void dijkstra(int verticeInicial) {
+  // Inicia as distancias como valores suficientemente grandes e visitados como falso
+  for (int i = 0; i < V; i++) {
+    distancias[i] = INT_MAX;
+    visitados[i] = 0;
+  }
+
+  distancias[verticeInicial] = 0; // Inicia o Vertice da distancia inicial como 0;
+  for (int i = 0; i < V - 1; i++) {
+    int verticeAtual = encontrarVerticeMaisProximo();
+    visitados[verticeAtual] = 1;
+
+    for (int j = 0; j < V; j++) {
+      if (!visitados[j] && grafo[verticeAtual][j] != 0 && distancias[verticeAtual] != INT_MAX && distancias[verticeAtual] + grafo[verticeAtual][j] < distancias[j]) {
+        distancias[j] = distancias[verticeAtual] + grafo[verticeAtual][j];
+      }
+    }
+  }
+}
+
+
+int main() {
+  int verticeInicial = 0;
+
+  dijkstra(verticeInicial);
+
+  printf("Vértice\tDistância Mínima\n");
+  for (int i = 0; i < V; i++) {
+    printf("%d\t%d\n", i, distancias[i]);
+  }
+
+  return 0;
+}
